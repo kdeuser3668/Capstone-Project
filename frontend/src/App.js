64485a4ce74747a3bdf7bd4fa21fb231babe68c4
Sydney Cable <sydneycable@gmail.com>
2025-09-30 14:43:07 -0500

    Fixed the double App function definition so login should pop up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import LoginForm from './LoginForm';
 
-function App() {
-  return (
-    <div className="App">
-      <LoginForm />
-    </div>
-  );
-}
 
 function App() {
   return (
     <div className="App">
+      <LoginForm />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
