ec7dc78de8d96568fb8c4f650da8a8154365fef7
Sydney Cable <sydneycable@gmail.com>
2025-10-09 19:52:04 -0500

    added sidebar, need to add more pages for more buttons

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,6 +1,7 @@
 ///PlannerPal dashboard
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 
 function Dashboard(){
@@ -8,35 +9,33 @@ function Dashboard(){
     var today = new Date();
 
     // Array of month names
-    var monthNames = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-    ];
-
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var dd = today.getDate(); // Day of the month
     var mm = monthNames[today.getMonth()]; // Month name
     var yyyy = today.getFullYear();
 
     const theDate = mm + ' ' + dd + ', ' + yyyy;
 
-    const handleLogout = async (e) => {
-        e.preventDefault();
-        
-        if (handleLogout){
-            localStorage.setItem('isAuthenticated', 'false');
-            navigate('/')
-        }
-    }; 
+
 return (
-    <div>
-        <h1>Dashboard</h1>
-        <h3 style={styles.h3}>{theDate}</h3>
-        <button type="submit" onClick={handleLogout} style={styles.button}>Log Out</button>
+    <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div style={styles.page}>
+            <h1>Dashboard</h1>
+            <h3 style={styles.h3}>{theDate}</h3>
+        </div>
     </div>
     )
 };
 
 const styles = {
+    page: {
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "200vh",
+        backgroundColor:"white",
+    },
     button:{
         padding: ".5rem",
         fontSize: "1rem",
@@ -48,6 +47,7 @@ const styles = {
     },
     h3:{
         fontWeight: "normal",
+
     },
 }
 
