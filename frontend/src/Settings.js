45979ddf18ad603c4054ef3535ceae7d6dec3efd
Sydney Cable <sydneycable@gmail.com>
2025-10-10 13:58:30 -0500

    Updated Sidebar.js to navigate to each page

--- frontend/src/Settings.js
+++ frontend/src/Settings.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Sidebar from './Sidebar';
 
 
-function Dashboard(){
+function Settings(){
     const navigate = useNavigate();
     var today = new Date();
 
@@ -52,4 +52,4 @@ const styles = {
     }
 }
 
-export default Dashboard;
+export default Settings;
