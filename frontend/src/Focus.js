2abfa8dc8d605048893259f96eafd40506464610
Lani <LMNWKX@umsystem.edu>
2025-10-10 15:10:44 -0500

    added timer function

--- frontend/src/Focus.js
+++ frontend/src/Focus.js
@@ -27,6 +27,37 @@ return (
     )
 };
 
+const Timer = () => {
+    const [days, setDays] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+    const deadline = "December, 31, 2025"
+
+    const getTime = () => {
+        const time = Date.parse(deadline) - Date.now();
+
+        setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+        setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+        setMinutes(Math.floor((time / 1000) % 60));
+        setSeconds(Math.floor((time / 1000) % 60));
+    }
+
+    useEffect(() => {
+        const interval = setINterval(() => getTime(deadline), 1000);
+
+        return () => clearInterval(internal);
+    }, []);
+
+    return (
+        <div className="timer">
+        </div> 
+    );
+};
+
+Timer()
+
 const styles = {
     page: {
         justifyContent: "center",
