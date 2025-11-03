2cc9111428d62ab017efb2bc95fcd8a0241d0c72
Anna Baranowski <111789609+annabaranowski@users.noreply.github.com>
2025-11-03 14:59:13 -0600

    Actual fix

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import signupRoutes from './signup.js';
 import loginRoutes from './login.js';
+import calendarRoutes from "./calendar.js";
 
 const router = express.Router();
 
 // Route groups
 router.use('/signup', signupRoutes);
 router.use('/login', loginRoutes);
+router.use("/api/calendar", calendarRoutes);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
-import { db } from "./db.js";
+import { pool } from "./db.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
