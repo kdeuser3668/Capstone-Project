4f51f6c5d581ec27f40f65fd78bbce2bc6e767c1
kdeuser3668 <112040794+kdeuser3668@users.noreply.github.com>
2025-11-04 14:54:25 -0600

    Remove default port fallback in server.js

--- backend/server.js
+++ backend/server.js
@@ -8,5 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use("/", routes);
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
