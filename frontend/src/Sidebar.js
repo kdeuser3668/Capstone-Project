a2155a8d6d9706280eb150113d2ca36e1a39698d
Sydney Cable <sydneycable@gmail.com>
2025-11-04 14:58:19 -0600

    added collapsable sidebar and b itton

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,7 +1,11 @@
 //eventually want to make this collapsable
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 const Sidebar = () => {
+    
+  const [isCollapsed, setIsCollapsed] = useState(false)
+    
     const linkStyle = {
     display: "block",
     padding: "1rem 1.5rem",
@@ -21,19 +25,32 @@ const Sidebar = () => {
       backgroundColor: "var(--sidebar-color, #f5f5f5)",
       padding: "1rem",
       boxSizing: "border-box",
-      border: "black",
+      flexDirection: "column",
+      borderRight: "1px solid #ccc",
+      //transition: "width 0.3s ease",
+      width: isCollapsed ? "0px" : "150px",
+      overflow: "hidden",
+      position: "relative",
     }}>
-      <h2 style={{ color: "var(--text-color)", marginBottom: "2rem" }}>PlannerPal</h2>
 
+      <h2 style={{ color: "var(--text-color)", marginBottom: "2rem" }}>PlannerPal</h2>
+      <button
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        className="toggleButtonStyle"
+        title={isCollapsed ? "Expand" : "Collapse"}
+      >{isCollapsed ? "›" : "‹"}</button>
       <NavLink to="/dashboard" style={({ isActive }) => isActive ? { ...linkStyle, ...activeStyle } : linkStyle}>Dashboard</NavLink>
       <NavLink to="/calendar" style={({ isActive }) => isActive ? { ...linkStyle, ...activeStyle } : linkStyle}>Calendar</NavLink>
       <NavLink to="/focus" style={({ isActive }) => isActive ? { ...linkStyle, ...activeStyle } : linkStyle}>Focus</NavLink>
       <NavLink to="/notes" style={({ isActive }) => isActive ? { ...linkStyle, ...activeStyle } : linkStyle}>Notes</NavLink>
       <NavLink to="/tasks" style={({ isActive }) => isActive ? { ...linkStyle, ...activeStyle } : linkStyle}>Tasks</NavLink>
       <NavLink to="/settings" style={({ isActive }) => isActive ? { ...linkStyle, ...activeStyle } : linkStyle}>Settings</NavLink>
+
+      <div style={{ }}>
       <NavLink to="/" style={({ isActive }) => isActive ? { ...linkStyle, ...activeStyle } : linkStyle}>Log Out</NavLink>
-    
+      </div>
     </div>
+
   );
 };
 
