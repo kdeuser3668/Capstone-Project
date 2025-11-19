b0deafb5a5b66ae916d49034ea9dcd574c658513
Sydney Cable <sydneycable@gmail.com>
2025-11-19 15:24:49 -0600

    made changes to courses.js

--- frontend/src/Courses.js
+++ frontend/src/Courses.js
@@ -2,20 +2,38 @@ import { useEffect, useState } from "react";
 import Dashboard from "./Dashboard";
 
 export function Courses(){
-    const [course_name, setCourseName] = useState("")
-    const [course_code, setCourseCode] = useState("")
-    const [instructor_name, setInstructorName] = useState("")
-    const [course_semester, setCourseSemester] = useState("")
-    const [courseId, setCourseId] = useState()
+    const [course_name, setCourseName] = useState("");
+    const [course_code, setCourseCode] = useState("");
+    const [instructor_name, setInstructorName] = useState("");
+    const [course_semester, setCourseSemester] = useState("");
 
-    const newCourse = {course_name, course_code, instructor_name, course_semester}
+    const [courseId, setCourseId] = useState(1);
+    const [courses, setCourses] = useState([]);
+    const [showPopup, setShowPopup] = useState(false);
 
+    const addCourse = () => {
+        const newCourse = {
+            course_name, 
+            course_code, 
+            instructor_name, 
+            course_semester
+        };
 
+        setCourses([...courses, newCourse]);
+        setCourseId(courseId + 1);
+
+        setCourseName("");
+        setCourseCode("");
+        setInstructorName("");
+        setCourseSemester("");
+    }
+    setShowPopup(true);
 
     return(
         <h3>{username || "User"}'s Courses</h3>
-        
+
     );
+
 };
 
 export default Courses;
