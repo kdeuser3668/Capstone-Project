234b22e42679b3ff69b238c78dacf1eb88759810
Sydney Cable <sydneycable@gmail.com>
2025-11-19 13:38:23 -0600

    added to courses.js

--- frontend/src/Courses.js
+++ frontend/src/Courses.js
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
+import Dashboard from "./Dashboard";
 
 export function Courses(){
-    const [course, setCourse] = useState("")
+    const [course_name, setCourseName] = useState("")
+    const [course_code, setCourseCode] = useState("")
+    const [instructor_name, setInstructorName] = useState("")
+    const [course_semester, setCourseSemester] = useState("")
     const [courseId, setCourseId] = useState()
 
+    const newCourse = {course_name, course_code, instructor_name, course_semester}
 
-    return
 
-    };
 
+    return(
+        <h3>{username || "User"}'s Courses</h3>
+
+    );
+};
 
 export default Courses;
